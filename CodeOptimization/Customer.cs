c331e9f49c3fc368dfd4408946c6bddf1054d541
suleyman <jdksuleyman@gmail.com>
2023-11-08 15:57:19 +0300

    Add project files.

---
+++ CodeOptimization/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        //Kullanıcı aktif ise %10 indirme - pasifse indirimden yararlanamıyor.
+        public decimal CalculateDiscount()
+        {
+            return IsActive ? 0.1M : 0;
+        }
+    }
+}
---
+++ CodeOptimization/CustomerRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace Example_1
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        IEnumerable<T> GetAll();
+    }
+
+    public class CustomerRepository : IRepository<Customer>, ICustomerActions
+    {
+        private List<Customer> customers;
+
+        public CustomerRepository()
+        {
+            customers = new List<Customer>();
+        }
+
+
+        public void Add(Customer customer)
+        {
+            if (customers.Any(c => c.Id == customer.Id))
+            {
+                throw new Exception("Dublicate ID");
+            }
+            customers.Add(customer);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return customers.ToList();
+        }
+
+        public void ArchiveCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
